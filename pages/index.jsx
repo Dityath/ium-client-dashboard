035598d8018c59f67388b36592fe1d52a79714b1
dityath <dityaathallah107@gmail.com>
2021-11-20 21:38:43 +0700

    login page

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/layout";
+
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Text>Hello kontol</Text>
+    </>
+  );
+}
---
+++ pages/login.jsx
@@ -0,0 +1,64 @@
+import {
+  Box,
+  Center,
+  Heading,
+  Container,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+
+function Login() {
+  const [showPass, setShowPass] = useState(false);
+
+  return (
+    <Center backgroundColor="orange.100" w="100%" h="100vh">
+      <Container
+        maxW="96"
+        backgroundColor="white"
+        centerContent
+        borderRadius="2xl"
+        boxShadow="lg"
+        padding="5"
+      >
+        <Heading as="h1" size="xl">
+          Login
+        </Heading>
+        <Heading as="h2" size="xs">
+          Silahkan masuk untuk melanjutkan
+        </Heading>
+        <Input
+          type="text"
+          focusBorderColor="red.400"
+          placeholder="Username"
+          size="md"
+          maxW="80"
+        />
+        <InputGroup size="md" maxW="80">
+          <Input
+            pr="4.5rem"
+            type={showPass ? "text" : "password"}
+            placeholder="Masukkan password"
+            focusBorderColor="red.400"
+          />
+          <InputRightElement width="4.5rem">
+            <Button
+              h="1.75rem"
+              size="sm"
+              onClick={() => setShowPass(!showPass)}
+            >
+              {showPass ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        <Button type="submit" w="80">
+          Masuk
+        </Button>
+      </Container>
+    </Center>
+  );
+}
+
+export default Login;
